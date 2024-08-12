521ca714b2848c9f4ad8967ea745ab73ffd587b3
foysauce <mr.foy488@gmail.com>
2024-08-12 16:38:12 -0400

    work

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name: 'Projects', link: '#projects'},
+        {name: 'About Me', link: '#about'},
+        // {name: 'Projects', link: '#projects'},
+    ]
+</script>
+
+<header>
+    <h1>
+        <b>Matthew</b> Foy
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+            <a>
+                <p>
+                    {tab.name}
+                </p>
+            </a>
+        {/each}
+    </div>
+</header>
