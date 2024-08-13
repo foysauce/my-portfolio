4a110dca1602d6fdda32dec2c1166c6a469c9a6c
foysauce <mr.foy488@gmail.com>
2024-08-12 21:00:14 -0400

    Main Page and Header
    Its starting to look like an actual webstie!

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,24 +1,47 @@
 <script>
     export let y;
 
-    let tabs = [
+    export let tabs = [
         {name: 'Projects', link: '#projects'},
         {name: 'About Me', link: '#about'},
         // {name: 'Projects', link: '#projects'},
     ]
 </script>
 
-<header>
-    <h1>
-        <b>Matthew</b> Foy
+<header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid" + 
+    (
+        y > 0
+        ? 'py-4 bg-slate-950 border-violet-950'
+        : 'py-6 bg-transparent border-transparent')}
+>
+    <h1 class="font-medium ">
+        <b class="font-bold poppins">Matthew</b> <span>Foy</span>
     </h1>
-    <div class="">
+    <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
-            <a>
-                <p>
-                    {tab.name}
-                </p>
+            <a
+                href={tab.link}
+                class="duration-200 hover:text-violet-400"
+                target={index === 2 ? "_blank" : ""}
+            >
+                <p>{tab.name}</p>
             </a>
         {/each}
+        <!-- <button
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-violet-600 text-white"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Login</h4>
+        </button> -->
+        <button
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Get in touch</h4>
+        </button>
     </div>
 </header>
